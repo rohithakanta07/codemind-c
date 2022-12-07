41050ee95e3b463fb7588eb7c18ad5f9cfe0ceca
rohithakanta07 <119839280+rohithakanta07@users.noreply.github.com>
2022-12-07 10:06:03 +0530

    #include<stdio.h> int main () {     int p1P1,p2P2,p3P3,p4P4;     scanf("%d%d%d%d",&p1P1,&p2P2,&p3P3,&p4P4);     if(p1P1,p2P2,p3P3,p4P4>=10)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int p1P1,p2P2,p3P3,p4P4;
+    scanf("%d%d%d%d",&p1P1,&p2P2,&p3P3,&p4P4);
+    if(p1P1,p2P2,p3P3,p4P4>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
