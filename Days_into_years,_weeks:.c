acca731d8b73c0eb6e5ebf259c34a46bc2daee24
rohithakanta07 <119839280+rohithakanta07@users.noreply.github.com>
2022-12-27 11:25:10 +0530

    #include<stdio.h> int days( int d) {     int w,y;     y=d/365;     w=(d%365)/7;     printf("%d %d ",y,w);     return (w,y); } int main() {     int d;     scanf("%d",&d);     days(d); }

---
+++ Days_into_years,_weeks:.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int days( int d)
+{
+    int w,y;
+    y=d/365;
+    w=(d%365)/7;
+    printf("%d
+%d
+",y,w);
+    return (w,y);
+}
+int main()
+{
+    int d;
+    scanf("%d",&d);
+    days(d);
+}
