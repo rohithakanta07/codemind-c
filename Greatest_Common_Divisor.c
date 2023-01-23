06207635ad09c3772332b04fc840addec9306360
rohithakanta07 <119839280+rohithakanta07@users.noreply.github.com>
2023-01-23 21:13:24 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b,min;     scanf("%d%d",&a,&b);     min=(a<b)?a:b;     for(min;min>0;min--)     {         if(a%min==0 && b%min==0)         {             printf("%d",min);             break;         }     } }

---
+++ Greatest_Common_Divisor.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,min;
+    scanf("%d%d",&a,&b);
+    min=(a<b)?a:b;
+    for(min;min>0;min--)
+    {
+        if(a%min==0 && b%min==0)
+        {
+            printf("%d",min);
+            break;
+        }
+    }
+}
