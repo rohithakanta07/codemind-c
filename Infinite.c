19bd8fdb8a8fdc24d1b6c480fda55b30bb6e85e6
rohithakanta07 <119839280+rohithakanta07@users.noreply.github.com>
2023-01-23 21:09:15 +0530

    #include<stdio.h> int main() {     int b,i;     while(b!=-1)     {    scanf("%d",&b);     if(b!=-1)     {         printf("%d ",b*b);     }     else     {     break;     }     } }

---
+++ Infinite.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int b,i;
+    while(b!=-1)
+    {
+   scanf("%d",&b);
+    if(b!=-1)
+    {
+        printf("%d
+",b*b);
+    }
+    else
+    {
+    break;
+    }
+    }
+}
